35d413bd1acbd16845a66e0379eb48cf89fe1a76
Kareem <k2gees@gmail.com>
2020-09-09 13:31:02 -0500

    initial framework and containers set up

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Overview
 
   Here is where we will make the uiltimate movie search app! We will be able to quickly find any movie based on it's title or an actor played in the movie using APIs
+
+##Screenshots 
+![Initial Mock up](./images/ums-mockup)
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    //Youtube API Key = AIzaSyBnRzgL5l_vrUMhVvZ-uzyiPxmfuiTECJE
+
+    // Youtube Link still needs work
+    var youtubeURL = "https://www.googleapis.com/youtube/v3/search?q=jawsd&key=AIzaSyBnRzgL5l_vrUMhVvZ-uzyiPxmfuiTECJE"
+
+    // OMDB API Key = 8db38eaf9dmsh0f91b07783026d4p1d126fjsn14a66fa92fcd
+    //
+
+    var imdbURL = "https://imdb8.p.rapidapi.com/actors/get-bio?nconst=nm0001667"
+})

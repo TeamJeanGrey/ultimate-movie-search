e46d946ad4e77615a544fb4094f7ed172925bdab
Seth Uschuk <suschuk24@gmail.com>
2020-09-12 12:07:06 -0500

    added HTML elements and CSS flexboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+var textArea = document.querySelector("#text-area")
+
 $(document).ready(function () {
     //Youtube API Key = AIzaSyBnRzgL5l_vrUMhVvZ-uzyiPxmfuiTECJE
+    // Seth's YT API = AIzaSyAoXr7-PmcQGq0Mh_vrEVkxiHjQrsKc_NI
 
-    // Youtube Link still needs work
-    var youtubeURL = "https://www.googleapis.com/youtube/v3/search?q=jawsd&key=AIzaSyBnRzgL5l_vrUMhVvZ-uzyiPxmfuiTECJE"
+    // YChanged from youtibne to wiki page link
+    var youtubeURL = "https://www.googleapis.com/youtube/v3/search?q="+ textArea +"+trailer&key=AIzaSyAoXr7-PmcQGq0Mh_vrEVkxiHjQrsKc_NI"
 
+    console.log(youtubeURL)
     // OMDB API Key = 8db38eaf9dmsh0f91b07783026d4p1d126fjsn14a66fa92fcd
     //
 

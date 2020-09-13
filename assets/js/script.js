3dc32253a5daf7208f67ef0eb18b036df3cb1687
Seth Uschuk <suschuk24@gmail.com>
2020-09-13 14:27:04 -0500

    major changes to html and js pages, added second html for movie details

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,29 +13,81 @@ var name = searchInputEl.value.trim();
 // Seth's IMDB rapidAPI key: 7caae45cdcmshfed4878de75f005p14c8bbjsnecd1e19ab576
 //  var imdbURL = "https://imdb8.p.rapidapi.com/actors/get-bio?nconst=nm0001667"
 
-var formSubmitHandler = function (event) {
-    event.preventDefault();
-    if (name) {
-        // searchInputEl.value = "";
-    } else {
-        // We're going to have to figure out how to change this to a modal, we're not allowed to use alerts. 
-        alert("Please enter an actor name.");
+$(document).ready(function () {
+    $("#actor-name").click(function (e) {
+        console.log($("#actor-name").val())
+        e.preventDefault()
+        var validate = Validate();
+        $("#message").html(validate);
+        if (validate.length == 0) {
+            $.ajax({
+                url: "http://imdb.wemakesites.net/api/search?q=" + $("#searchInput").val(),
+                data: { "api_key": "9c4d2620-32a1-4f4a-abd0-c88b008f383e" },
+                crossDomain: true,
+                dataType: "jsonp",
+                success: function (result, status, xhr) {
+                    var resultHtml = $("<div class=\"resultDiv\"><p>Names</p>");
+                    for (i = 0; i < result["data"]["results"]["names"].length; i++) {
+                        resultHtml.append("<div class=\"result\" resourceId=\"" + result["data"]["results"]["names"][i]["id"] + "\">" + "<img src=\"" + result["data"]["results"]["names"][i]["thumbnail"] + "\" />" + "<p><a>" + result["data"]["results"]["names"][i]["title"] + "</a></p></div>")
+                    }
+
+                    resultHtml.append("<p>Titles</p>");
+                    for (i = 0; i < result["data"]["results"]["titles"].length; i++) {
+                        resultHtml.append("<div class=\"result\" resourceId=\"" + result["data"]["results"]["titles"][i]["id"] + "\">" + "<img src=\"" + result["data"]["results"]["titles"][i]["thumbnail"] + "\" />" + "<p><a>" + result["data"]["results"]["titles"][i]["title"] + "</a></p></div>")
+                    }
+
+                    resultHtml.append("</div>");
+
+                    $("#message").html(resultHtml);
+                },
+                error: function (xhr, status, error) {
+                    $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+                }
+            });
+        }
+    });
+
+    function Validate() {
+        var errorMessage = "";
+        if ($("#searchInput").val() == "") {
+            errorMessage += "► Enter Search Text";
+        }
+        return errorMessage;
     }
 
-    return name
-};
-var imdbActorBio = function (event) {
-    // concatinated URL link: "https://imdb8.p.rapidapi.com/actors/get-bio?=nm0001667" + name +"&rapidapi-key=7caae45cdcmshfed4878de75f005p14c8bbjsnecd1e19ab576"
-    var apiUrl = "https://imdb8.p.rapidapi.com/actors/get-bio?&nconst=nm0001667&rapidapi-key=7caae45cdcmshfed4878de75f005p14c8bbjsnecd1e19ab576"
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                displayBio(data, name)
-                console.log("Actor's bio content and image will append here")
-            })
-        } else { console.log("no result") }
-    })
-}
+    $(document).ajaxStart(function () {
+        $(".textAlignCenter img").show();
+    });
+
+    $(document).ajaxStop(function () {
+        $(".textAlignCenter img").hide();
+    });
+
+});
+
+// var formSubmitHandler = function (event) {
+//     event.preventDefault();
+//     if (name) {
+//         // searchInputEl.value = "";
+//     } else {
+//         // We're going to have to figure out how to change this to a modal, we're not allowed to use alerts. 
+//         alert("Please enter an actor name.");
+//     }
+
+//     return name
+// };
+// var imdbActorBio = function (event) {
+//     // concatinated URL link: "https://imdb8.p.rapidapi.com/actors/get-bio?=nm0001667" + name +"&rapidapi-key=7caae45cdcmshfed4878de75f005p14c8bbjsnecd1e19ab576"
+//     var apiUrl = "https://imdb8.p.rapidapi.com/actors/get-bio?&nconst=nm0001667&rapidapi-key=7caae45cdcmshfed4878de75f005p14c8bbjsnecd1e19ab576"
+//     fetch(apiUrl).then(function (response) {
+//         if (response.ok) {
+//             response.json().then(function (data) {
+//                 displayBio(data, name)
+//                 console.log("Actor's bio content and image will append here")
+//             })
+//         } else { console.log("no result") }
+//     })
+// }
 
 // var imdbActorKnownFor = function(event) {
 //     var apiUrl = "https://imdb8.p.rapidapi.com/actors/get-kown-for?" + name +"&rapidapi-key=7caae45cdcmshfed4878de75f005p14c8bbjsnecd1e19ab576"
@@ -61,14 +113,14 @@ var imdbActorBio = function (event) {
 //     })
 // }
 
-var displayBio = function () {
-    // get image from IMDB API
-    var imgUrl = "https://m.media-amazon.com/images/M/MV5BMGEwYzJhOTctNjZkMy00NTZjLWE3MTctNGU2ZGRmODNiZmNiXkEyXkFqcGdeQXVyOTQyNzIyMDM@._V1_.jpg"
-    actorImgEl.setAttribute("src", imgUrl)
-    console.log(actorImgEl)
-    // get bio from IMDB API
-    var actorBio = client.miniBios[0].text
-    console.log(actorBio)
+// var displayBio = function () {
+//     // get image from IMDB API
+//     var imgUrl = "https://m.media-amazon.com/images/M/MV5BMGEwYzJhOTctNjZkMy00NTZjLWE3MTctNGU2ZGRmODNiZmNiXkEyXkFqcGdeQXVyOTQyNzIyMDM@._V1_.jpg"
+//     actorImgEl.setAttribute("src", imgUrl)
+//     console.log(actorImgEl)
+//     // get bio from IMDB API
+//     var actorBio = client.miniBios[0].text
+//     console.log(actorBio)
 
 }
 
@@ -81,7 +133,6 @@ var displayBio = function () {
 // }
 
 function search() {
-    execute()
     imdbActorBio()
     // imdbActorKnownFor()
     // imdbActorAwards()
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,15 +1,101 @@
-var actorName;
+$(document).ready(function () {
+    $("#search-container").submit(function (event) {
+        searchName = $('#movie-name').val().trim()
+        event.preventDefault()
+        saveInput(searchName)
+        getMovies(searchName)
+        getVideo();
+            });
+    });
 
-$("#search-container").submit(function (event) {
-    event.preventDefault();
-    actorName = $('#actor-name').val();
+function getMovies(searchName) {
+    axios.get('https://www.omdbapi.com?s=' + searchName + '&apikey=e8893681')
+    .then((response) => {
+        console.log(response)
+        var movies = response.data.Search
+        var output = ''
+        $.each(movies, (index, movie) => {
+            output += `
+            
+            <div class="col s3">
+                <div class="well text-center">
+                    <img src="${movie.Poster}">
+                    <h5>${movie.Title}</h5>
+                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                </div>
+            </div>
+            
+            `;
+        });
 
-    var query = actorName + " newest trailer";
+        $('#movies').html(output)
+    })
+    .catch((error)=> {
+        console.log(error)
+    })
+}
 
-    getVideo(query);
-});
+function movieSelected(id) {
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html'
+    return false
+}
 
-function getVideo(query) {
+function saveInput(searchName) {
+    sessionStorage.setItem("movie-name", searchName)
+    
+}
+
+function getMovie() {
+    var movieId = sessionStorage.getItem('movieId')
+    axios.get('https://www.omdbapi.com?i=' + movieId + '&apikey=e8893681')
+    .then((response) => {
+        var movie = response.data
+        console.log(response.data.Actors)
+        var output = `
+            <div class="row">
+                <div class="col s4">
+                    <img src="${movie.Poster}" class="thumbnail">
+                </div>
+                <div class="col s8">
+                    <h2>${movie.Title}</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item">Genre:${movie.Genre}</li>
+                            <li class="list-group-item">Released:${movie.Released}</li>
+                            <li class="list-group-item">IMDB Rating:${movie.imdbRating}</li>
+                            <li class="list-group-item">Director:${movie.Director}</li>
+                            <li class="list-group-item">Writer:${movie.Writer}</li>
+                            <li class="list-group-item">Actors:${movie.Actors}</li>
+                        </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="well">
+                    <h3>Plot</h3>
+                    ${movie.Plot}
+                    <hr>
+                    <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class=btn btn-primary">View IMDB Page</a>
+                    <a href="landing.html" class="btn btn-default">Back to Search Page</a>
+                </div>
+            </div>
+        `;
+        $('#movie').html(output)
+    })
+    .catch((error)=> {
+        console.log(error)
+    })
+}
+// $("#search-container").submit(function (event) {
+//     event.preventDefault();
+//     var searchMovieNameEl = sessionStorage.getItem("movie-name") + " newest trailer";
+
+//     getVideo(searchMovieNameEl);
+// });
+
+
+function getVideo() {
+    console.log("running get video")
+    var query = sessionStorage.getItem("movie-name") + " newest trailer";
     $.ajax({
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
@@ -22,6 +108,7 @@ function getVideo(query) {
             videoEmbeddable: true,
         },
         success: function (data) {
+            console.log(data)
             embedVideo(data)
         },
         error: function (response) {
@@ -33,3 +120,5 @@ function getVideo(query) {
 function embedVideo(data) {
     $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId + "?autoplay=1&;enablejsapi=1")
 }
+
+

dcdf455227b4a2224181724129042d68f3fc968f
Jordan <Joch@Jordans-MacBook-Pro.local>
2020-09-14 03:50:02 -0700

    fixed YT api and added some css

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,34 +5,40 @@ $(document).ready(function () {
         saveInput(searchName)
         getMovies(searchName)
         getVideo();
-            });
     });
+});
 
 function getMovies(searchName) {
     axios.get('https://www.omdbapi.com?s=' + searchName + '&apikey=e8893681')
-    .then((response) => {
-        console.log(response)
-        var movies = response.data.Search
-        var output = ''
-        $.each(movies, (index, movie) => {
-            output += `
+        .then((response) => {
+            console.log(response)
+            var movies = response.data.Search
+            var output = ''
+            $.each(movies, (index, movie) => {
+                output += `
             
-            <div class="col s3">
-                <div class="well text-center">
-                    <img src="${movie.Poster}">
-                    <h5>${movie.Title}</h5>
-                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            <div class="col l3 m4 s12">
+                <div class="card large" >
+                    <div class="card-image">
+                        <img class="responsive-img" src="${movie.Poster}">
+                    </div>
+                    <div class="card-content">
+                        <h5>${movie.Title}</h5>
+                    </div>
+                    <div class="card-action center">
+                        <a onclick="movieSelected('${movie.imdbID}'); setMovieSelected('${movie.Title}')" class="btn btn-primary" href="#">Movie Details</a>
+                    </div>
                 </div>
             </div>
             
             `;
-        });
+            });
 
-        $('#movies').html(output)
-    })
-    .catch((error)=> {
-        console.log(error)
-    })
+            $('#movies').html(output)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
 
 function movieSelected(id) {
@@ -41,18 +47,22 @@ function movieSelected(id) {
     return false
 }
 
+function setMovieSelected(title) {
+    sessionStorage.setItem('movie-title', title)
+}
+
 function saveInput(searchName) {
     sessionStorage.setItem("movie-name", searchName)
-    
+
 }
 
 function getMovie() {
     var movieId = sessionStorage.getItem('movieId')
     axios.get('https://www.omdbapi.com?i=' + movieId + '&apikey=e8893681')
-    .then((response) => {
-        var movie = response.data
-        console.log(response.data.Actors)
-        var output = `
+        .then((response) => {
+            var movie = response.data
+            console.log(response.data.Actors)
+            var output = `
             <div class="row">
                 <div class="col s4">
                     <img src="${movie.Poster}" class="thumbnail">
@@ -79,11 +89,11 @@ function getMovie() {
                 </div>
             </div>
         `;
-        $('#movie').html(output)
-    })
-    .catch((error)=> {
-        console.log(error)
-    })
+            $('#movie').html(output)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
 }
 // $("#search-container").submit(function (event) {
 //     event.preventDefault();
@@ -95,12 +105,14 @@ function getMovie() {
 
 function getVideo() {
     console.log("running get video")
-    var query = sessionStorage.getItem("movie-name") + " newest trailer";
+    var query = sessionStorage.getItem("movie-title") + " trailer";
     $.ajax({
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: {
-            key: 'AIzaSyB7jf8WLIKIDfZ5iuVWr7m3McWZMoWmYE0',
+            // key1 = AIzaSyB7jf8WLIKIDfZ5iuVWr7m3McWZMoWmYE0
+            // key2 = AIzaSyAgp2vMl59orNeECqvXmizUYVk9HO4dABo
+            key: 'AIzaSyAgp2vMl59orNeECqvXmizUYVk9HO4dABo',
             q: query,
             part: 'snippet',
             maxResults: 1,
@@ -120,5 +132,3 @@ function getVideo() {
 function embedVideo(data) {
     $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId + "?autoplay=1&;enablejsapi=1")
 }
-
-

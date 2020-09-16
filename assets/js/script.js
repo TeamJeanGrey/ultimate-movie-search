9aa501c84693f76ab874feeb7850ac5269f41958
Seth Uschuk <suschuk24@gmail.com>
2020-09-15 19:16:18 -0500

    commit to pull jordans code, minor comment changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-$(document).ready(function () {
-    $("#search-container").submit(function (event) {
-        searchName = $('#movie-name').val().trim()
-        event.preventDefault()
-        saveInput(searchName)
-        getMovies(searchName)
-        getVideo();
-            });
-    });
-
 function getMovies(searchName) {
     axios.get('https://www.omdbapi.com?s=' + searchName + '&apikey=e8893681')
     .then((response) => {
@@ -34,24 +24,25 @@ function getMovies(searchName) {
         console.log(error)
     })
 }
-
+// put user's text input request value in session storage
 function movieSelected(id) {
     sessionStorage.setItem('movieId', id);
     window.location = 'movie.html'
     return false
 }
-
+// save user's input to session storage
 function saveInput(searchName) {
     sessionStorage.setItem("movie-name", searchName)
     
 }
-
+// send to movie.html page, pull user's search from session storage to show movie details
 function getMovie() {
     var movieId = sessionStorage.getItem('movieId')
     axios.get('https://www.omdbapi.com?i=' + movieId + '&apikey=e8893681')
     .then((response) => {
         var movie = response.data
         console.log(response.data.Actors)
+        // create dynamic html items from omdb api
         var output = `
             <div class="row">
                 <div class="col s4">
@@ -100,7 +91,7 @@ function getVideo() {
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: {
-            key: 'AIzaSyB7jf8WLIKIDfZ5iuVWr7m3McWZMoWmYE0',
+            key: 'AIzaSyBnRzgL5l_vrUMhVvZ-uzyiPxmfuiTECJE',
             q: query,
             part: 'snippet',
             maxResults: 1,
